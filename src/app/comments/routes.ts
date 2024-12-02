05cb0d5102fdef7feab03859bcbadcc75fb55324
abdullah ahmed <abdallaroom25@gmail.com>
2024-12-02 12:23:11 +0200

    add replied routes

--- src/app/comments/routes.ts
+++ src/app/comments/routes.ts
@@ -2,8 +2,10 @@ import { router } from "@warlock.js/core";
 import { guarded } from "app/utils/router";
 import { createComment } from "./controllers/create-comment";
 import { updateComment } from "./controllers/update-comment";
+import { getRepliedComments } from "./controllers/get-replied-comments";
 
 
+router.get("/comment/replies/:id",getRepliedComments)
 
 guarded(()=>{
     router.post("/comment",createComment)
